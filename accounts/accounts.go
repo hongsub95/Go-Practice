9c1c25109596c55db95295dc961de7f9f828f184
hongsub <hong9506@gmail.com>
2022-09-16 01:05:54 +0900

    Error

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,5 +1,7 @@
 package accounts
 
+import "errors"
+
 //Account struct
 type Account struct {
 	owner   string
@@ -11,10 +13,22 @@ func NewAccount(owner string) *Account {
 	return &account
 }
 
+// 포인터(*)를 가져와야 한다. 복사본이 아닌 실제 account를 가져와야 해서
 func (a *Account) Deposit(amount int) {
 	a.balance += amount
 }
 
 func (a Account) Balance() int {
 	return a.balance
+}
+
+// 포인터(*)를 쓴 이유는 위와같음
+// nil은 null,none 이라고 생각하면 된다
+
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errors.New("잔고금액이 부족합니다")
+	}
+	a.balance -= amount
+	return nil
 }
--- main.go
+++ main.go
@@ -6,8 +6,14 @@ import (
 	"github.com/hongsub/learngo/Go-Practice/accounts"
 )
 
+// account:나의 계좌 balance:잔고 deposit:입금 withdraw:출금
 func main() {
 	account := accounts.NewAccount("hongsub")
 	account.Deposit(10)
+	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Println(account.Balance())
 }
