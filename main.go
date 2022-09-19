52f1f5f92ab8460537b75dea7fc98fb6d52d8971
hongsub <hong9506@gmail.com>
2022-09-19 16:53:45 +0900

    URL Status Check

--- main.go
+++ main.go
@@ -1,36 +1,45 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/hongsub/learngo/Go-Practice/mydict"
+	"net/http"
 )
 
-// account:나의 계좌 balance:잔고 deposit:입금 withdraw:출금
-/*
+var errRequestFailed = errors.New("Request Fail")
+
 func main() {
-	account := accounts.NewAccount("hongsub")
-	account.Deposit(10)
-	fmt.Println(account.Balance())
-	err := account.Withdraw(20)
-	if err != nil {
-		fmt.Println(err)
+	var results = make(map[string]string)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
-	fmt.Println(account.Balance())
+	results["hello"] = "Hello"
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	fmt.Println(results)
+
 }
-*/
 
-func main() {
-	dictionary := mydict.Dictionary{}
-	baseWord := "hello"
-	dictionary.Add(baseWord, "First")
-	dictionary.Add(baseWord, "First")
-	dictionary.Search(baseWord)
-	dictionary.Delete(baseWord)
-	word, err := dictionary.Search(baseWord)
-	if err != nil {
-		fmt.Println(err)
+// go lang standard library 참조
+func hitURL(url string) error {
+	fmt.Println("체크", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		return errRequestFailed
 	}
-	fmt.Println(word)
-
+	return nil
 }
