508ac25b00208f5e727413467c07f2806b2e8512
hongsub <hong9506@gmail.com>
2022-09-19 20:58:44 +0900

    final Channel practice

--- main.go
+++ main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"time"
-)
-
-/*
-import (
 	"errors"
 	"fmt"
 	"net/http"
 )
 
+type resultRequest struct {
+	url    string
+	status string
+}
+
 var errRequestFailed = errors.New("Request Fail")
 
 func main() {
-	var results = make(map[string]string)
+	results := make(map[string]string)
+	c := make(chan resultRequest)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -27,44 +27,26 @@ func main() {
 		"https://www.instagram.com/",
 		"https://academy.nomadcoders.co/",
 	}
-	results["hello"] = "Hello"
 	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
+		go hitURL(url, c)
 	}
-	fmt.Println(results)
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
 
 }
 
 // go lang standard library 참조
-func hitURL(url string) error {
-	fmt.Println("체크", url)
+// chan <-를 함으로써 이 channel은 보내기만 가능해짐 (구체적으로 명시하기 위해)
+func hitURL(url string, c chan<- resultRequest) {
 	resp, err := http.Get(url)
 	if err != nil || resp.StatusCode >= 400 {
-		return errRequestFailed
+		c <- resultRequest{url: url, status: "FAILED"}
+	} else {
+		c <- resultRequest{url: url, status: "OK"}
 	}
-	return nil
-}
-*/
-
-func main() {
-	c := make(chan string) //channel과 어떤 타입을 보낼건지
-	people := [6]string{"신", "홍", "섭", "개", "발", "자"}
-	for _, person := range people {
-		go isSexy(person, c)
-	}
-	for i := 0; i < len(people); i++ {
-		fmt.Print(i)
-		fmt.Println(<-c) //<-c는 blocking operation
-	}
-}
-
-//Channel을 이용하여 true 메세지를 보낸다, c로 true메세지를 보낸다
-func isSexy(person string, c chan string) {
-	time.Sleep(time.Second * 5)
-	c <- person
 }
